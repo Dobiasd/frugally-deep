4dcefa45721d70896aee22b29f02cece82bc076f
Tobias Hermann <editgym@gmail.com>
2016-07-14 22:36:13 +0200

    work with float images where possible

--- src/DeepVideoCompression.cpp
+++ src/DeepVideoCompression.cpp
@@ -11,9 +11,9 @@
 #include <random>
 
 
-matrix3d cv_bgr_img_to_matrix3d(const cv::Mat& img)
+matrix3d cv_bgr_img_float_to_matrix3d(const cv::Mat& img)
 {
-    assert(img.type() == CV_8UC3);
+    assert(img.type() == CV_32FC3);
     matrix3d m(size3d(
         3,
         static_cast<std::size_t>(img.cols),
@@ -22,12 +22,12 @@ matrix3d cv_bgr_img_to_matrix3d(const cv::Mat& img)
     {
         for (std::size_t x = 0; x < m.size().width(); ++x)
         {
-            cv::Vec3b col =
-                img.at<cv::Vec3b>(static_cast<int>(y), static_cast<int>(x));
+            cv::Vec3f col =
+                img.at<cv::Vec3f>(static_cast<int>(y), static_cast<int>(x));
             for (std::size_t c = 0; c < 3; ++c)
             {
                 m.set(c, y, x,
-                    static_cast<float>(col[static_cast<int>(c)]) / 256.0f);
+                    static_cast<float>(col[static_cast<int>(c)]));
             }
         }
     }
@@ -54,23 +54,19 @@ matrix3d cv_float_kernel_to_matrix3d(const cv::Mat& kernel,
     return m;
 }
 
-cv::Mat matrix3d_to_cv_bgr_img(const matrix3d& m)
+cv::Mat matrix3d_to_cv_bgr_img_float(const matrix3d& m)
 {
     assert(m.size().depth() == 3);
     cv::Mat img(
         static_cast<int>(m.size().height()),
         static_cast<int>(m.size().width()),
-        CV_8UC3);
+        CV_32FC3);
     for (std::size_t y = 0; y < m.size().height(); ++y)
     {
         for (std::size_t x = 0; x < m.size().width(); ++x)
         {
-            cv::Vec3b col(
-                static_cast<unsigned char>(std::max(0.0f, m.get(0, y, x) * 256)),
-                static_cast<unsigned char>(std::max(0.0f, m.get(1, y, x) * 256)),
-                static_cast<unsigned char>(std::max(0.0f, m.get(2, y, x) * 256))
-                );
-            img.at<cv::Vec3b>(static_cast<int>(y), static_cast<int>(x)) = col;
+            cv::Vec3f col(m.get(0, y, x), m.get(1, y, x), m.get(2, y, x));
+            img.at<cv::Vec3f>(static_cast<int>(y), static_cast<int>(x)) = col;
         }
     }
     return img;
@@ -103,27 +99,52 @@ layer_ptr cv_kernel_to_layer(const cv::Mat& cv_kernel)
 
 cv::Mat filter2DNet(const cv::Mat& img, const cv::Mat& cv_kernel)
 {
-    matrix3d in_vol = cv_bgr_img_to_matrix3d(img);
+    matrix3d in_vol = cv_bgr_img_float_to_matrix3d(img);
     std::vector<layer_ptr> layers = {cv_kernel_to_layer(cv_kernel)};
     conv_net net = conv_net(layers);
     auto out_vol = net.forward_pass(in_vol);
-    cv::Mat result = matrix3d_to_cv_bgr_img(out_vol);
+    cv::Mat result = matrix3d_to_cv_bgr_img_float(out_vol);
     return result;
 }
 
 cv::Mat filter2DsNet(const cv::Mat& img, const std::vector<cv::Mat>& cv_kernels)
 {
-    matrix3d in_vol = cv_bgr_img_to_matrix3d(img);
+    matrix3d in_vol = cv_bgr_img_float_to_matrix3d(img);
     auto layers = fplus::transform(cv_kernel_to_layer, cv_kernels);
     conv_net net = conv_net(layers);
     auto out_vol = net.forward_pass(in_vol);
-    cv::Mat result = matrix3d_to_cv_bgr_img(out_vol);
+    cv::Mat result = matrix3d_to_cv_bgr_img_float(out_vol);
+    return result;
+}
+
+cv::Mat uchar_img_to_float_img(const cv::Mat& uchar_img)
+{
+    assert(uchar_img.type() == CV_8UC3);
+    cv::Mat result;
+    uchar_img.convertTo(result, CV_32FC3, 1.0f/255.0f);
+    return result;
+}
+
+cv::Mat floatImgToUCharImg(const cv::Mat& floatImg)
+{
+    assert(floatImg.type() == CV_32FC3);
+    cv::Mat result;
+    floatImg.convertTo(result, CV_8UC3, 255.0f);
+    return result;
+}
+
+cv::Mat normalizeFloatImg(const cv::Mat& floatImg)
+{
+    assert(floatImg.type() == CV_32FC3);
+    cv::Mat result;
+    cv::normalize(floatImg, result, 0, 1, cv::NORM_MINMAX);
     return result;
 }
 
 int main()
 {
-    cv::Mat img = cv::imread("images/lenna_512x512.png", cv::IMREAD_COLOR);
+    cv::Mat img_uchar = cv::imread("images/lenna_512x512.png", cv::IMREAD_COLOR);
+    cv::Mat img = uchar_img_to_float_img(img_uchar);
 
     cv::Mat kernel_scharr_x(cv::Size(3,3), CV_32FC1, cv::Scalar(0));
     kernel_scharr_x.at<float>(0,0) =   3.0f / 32.0f;
@@ -139,11 +160,15 @@ int main()
     cv::Mat kernel_blur(cv::Size(3,3), CV_32FC1, cv::Scalar(1.0f/9.0f));
 
     cv::Mat filtered1;
-    filter2D(img, filtered1, -1, kernel_scharr_x);
+    filter2D(img, filtered1, CV_32FC3, kernel_scharr_x);
     filter2D(filtered1, filtered1, -1, kernel_scharr_x);
+    filtered1 = normalizeFloatImg(filtered1);
+    filtered1 = floatImgToUCharImg(filtered1);
     cv::imwrite("lenna_512x512_filtered1.png", filtered1);
 
     cv::Mat filtered2 = filter2DsNet(img, {kernel_scharr_x, kernel_scharr_x});
+    filtered2 = normalizeFloatImg(filtered2);
+    filtered2 = floatImgToUCharImg(filtered2);
     cv::imwrite("lenna_512x512_filtered2.png", filtered2);
 }
 

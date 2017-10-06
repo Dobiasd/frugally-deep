238cb8a1002b7e866cb39937f39c519f105092af
Dobiasd <editgym@gmail.com>
2017-10-06 16:38:27 +0100

    shared_float_vec

--- include/fdeep/common.hpp
+++ include/fdeep/common.hpp
@@ -7,17 +7,13 @@
 #pragma once
 
 #include <cmath>
+#include <memory>
 #include <vector>
 #include <string>
 #include <stdexcept>
 
 namespace fdeep { namespace internal
 {
-
-typedef float float_t;
-typedef std::vector<float_t> float_vec;
-typedef float_vec::const_iterator float_vec_const_it;
-const float_t pi = static_cast<float_t>(std::acos(-1));
 
 inline std::runtime_error error(const std::string& error)
 {
@@ -36,5 +32,105 @@ inline void assertion(bool cond, const std::string& error)
         raise_error(error);
     }
 }
+
+typedef float float_t;
+
+// shares unerlying data on copy and assignment
+class shared_float_vec
+{
+public:
+    typedef std::vector<float_t> vec_t;
+    typedef std::shared_ptr<vec_t> vec_ptr_t;
+    typedef vec_t::iterator iterator;
+    typedef vec_t::const_iterator const_iterator;
+    typedef vec_t::value_type value_type;
+
+    shared_float_vec(std::size_t size, float_t initial) :
+        data_(std::make_shared<std::vector<float_t>>(size, initial))
+    {
+    }
+
+    shared_float_vec(const shared_float_vec&) = default;
+    shared_float_vec& operator = (const shared_float_vec&) = default;
+
+    std::size_t size() const
+    {
+        return data_->size();
+    }
+    bool empty() const
+    {
+        return data_->empty();
+    }
+    iterator begin()
+    {
+        return data_->begin();
+    }
+    iterator end()
+    {
+        return data_->end();
+    }
+    const_iterator begin() const
+    {
+        return data_->begin();
+    }
+    const_iterator end() const
+    {
+        return data_->end();
+    }
+    void push_back(float_t x)
+    {
+        data_->push_back(x);
+    }
+    void reserve(std::size_t capacity)
+    {
+        data_->reserve(capacity);
+    }
+    float_t& operator[](std::size_t index)
+    {
+        return (*data_)[index];
+    }
+    const float_t& operator[](std::size_t index) const
+    {
+        return (*data_)[index];
+    }
+private:
+    vec_ptr_t data_;
+};
+
+struct shared_float_vec_back_insert_iterator : public std::back_insert_iterator<shared_float_vec>
+{
+    typedef std::back_insert_iterator<shared_float_vec> base_type;
+    explicit shared_float_vec_back_insert_iterator(shared_float_vec& v) :
+        base_type(v), v_ptr_(&v), pos_(0) {}
+    shared_float_vec_back_insert_iterator(const shared_float_vec_back_insert_iterator& other) :
+        base_type(*other.v_ptr_), v_ptr_(other.v_ptr_), pos_(other.pos_) {}
+    shared_float_vec_back_insert_iterator& operator=(const shared_float_vec_back_insert_iterator& other)
+    {
+        v_ptr_ = other.v_ptr_;
+        pos_ = other.pos_;
+        return *this;
+    }
+    shared_float_vec_back_insert_iterator& operator=(const float_t& x)
+    {
+        (*v_ptr_)[pos_] = x;
+        ++pos_;
+        return *this;
+    }
+    shared_float_vec_back_insert_iterator& operator=(float_t&& x)
+    {
+        (*v_ptr_)[pos_] = std::move(x);
+        ++pos_;
+        return *this;
+    }
+    shared_float_vec_back_insert_iterator& operator*() { return *this; }
+    shared_float_vec_back_insert_iterator& operator++() { return *this; }
+    shared_float_vec_back_insert_iterator operator++(int) { return *this; }
+private:
+    shared_float_vec* v_ptr_;
+    std::size_t pos_;
+};
+
+//typedef shared_float_vec float_vec;
+typedef std::vector<float_t> float_vec;
 
 } } // namespace fdeep, namespace internal

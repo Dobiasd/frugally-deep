b121d8d2d854964660b7afad3f9394482be2bd49
Dobiasd <editgym@gmail.com>
2017-10-07 19:27:19 +0200

    enable xception and inception models

--- keras_export/save_application_examples.py
+++ keras_export/save_application_examples.py
@@ -16,51 +16,36 @@ __maintainer__ = "Tobias Hermann, https://github.com/Dobiasd/frugally-deep"
 __email__ = "editgym@gmail.com"
 
 def save_xception(dir_path, file_name):
-    keras.applications.xception.Xception(
-        include_top=True, weights='imagenet', input_tensor=None,
-        input_shape=None, pooling=None, classes=1000).save(
+    keras.applications.xception.Xception(input_shape=(299, 299, 3)).save(
             os.path.join(dir_path, file_name + ".h5"))
 
 def save_vgg16(dir_path, file_name):
-    keras.applications.vgg16.VGG16(
-        include_top=True, weights='imagenet', input_tensor=None,
-        input_shape=None, pooling=None, classes=1000).save(
+    keras.applications.vgg16.VGG16().save(
             os.path.join(dir_path, file_name + ".h5"))
 
 def save_vgg19(dir_path, file_name):
-    keras.applications.vgg19.VGG19(
-        include_top=True, weights='imagenet',input_tensor=None,
-        input_shape=None, pooling=None, classes=1000).save(
+    keras.applications.vgg19.VGG19().save(
             os.path.join(dir_path, file_name + ".h5"))
 
 def save_resnet50(dir_path, file_name):
-    keras.applications.resnet50.ResNet50(
-        include_top=True, weights='imagenet', input_tensor=None,
-        input_shape=None, pooling=None, classes=1000).save(
+    keras.applications.resnet50.ResNet50().save(
             os.path.join(dir_path, file_name + ".h5"))
 
 def save_inceptionv3(dir_path, file_name):
-    keras.applications.inception_v3.InceptionV3(
-        include_top=True, weights='imagenet', input_tensor=None,
-        input_shape=None, pooling=None, classes=1000).save(
+    keras.applications.inception_v3.InceptionV3(input_shape=(299, 299, 3)).save(
             os.path.join(dir_path, file_name + ".h5"))
 
 def save_inceptionvresnetv2(dir_path, file_name):
-    keras.applications.inception_resnet_v2.InceptionResNetV2(
-        include_top=True, weights='imagenet', input_tensor=None,
-        input_shape=None, pooling=None, classes=1000).save(
+    keras.applications.inception_resnet_v2.InceptionResNetV2().save(
             os.path.join(dir_path, file_name + ".h5"))
 
 def save_mobilenet(dir_path, file_name):
-    keras.applications.mobilenet.MobileNet(
-        input_shape=None, alpha=1.0, depth_multiplier=1,
-        dropout=1e-3, include_top=True, weights='imagenet',
-        input_tensor=None, pooling=None, classes=1000).save(
+    keras.applications.mobilenet.MobileNet().save(
             os.path.join(dir_path, file_name + ".h5"))
 
 def main():
     if len(sys.argv) != 2:
-        print('usage: [output path]')
+        print('usage: [output dir]')
         sys.exit(1)
     else:
         dir_path = sys.argv[1]

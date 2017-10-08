b33527132ec919fa32a03e1458029d13fb502c00
Dobiasd <editgym@gmail.com>
2017-10-08 22:11:28 +0200

    softplus in TensorFlow fashion (with threshold)

--- include/fdeep/layers/softplus_layer.hpp
+++ include/fdeep/layers/softplus_layer.hpp
@@ -23,9 +23,15 @@ protected:
     {
         auto activation_function = [](float_type x) -> float_type
         {
-            // TensorFlow actually does something more complex:
             // https://github.com/tensorflow/tensorflow/blob/626808e4e4a83aafbb3809a30db57bb78e839040/tensorflow/core/kernels/softplus_op.h#L41
-            return static_cast<float_type>(log1p(std::exp(x)));
+            const float_type threshold =
+                std::log(std::numeric_limits<float_type>::epsilon()) + 2;
+            if (x > -threshold) // too_large
+                return x;
+            else if (x < threshold) // too_small
+                return std::exp(x);
+            else
+                return std::log1p(std::exp(x));
         };
         return transform_tensor3(activation_function, in_vol);
     }

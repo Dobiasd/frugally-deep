cfd25c13cdb6e71130a02890ee7db9ed8aa56b14
Dobiasd <editgym@gmail.com>
2017-11-22 11:11:17 +0000

    avoid conversion warnings when using double instead of float as float_type

--- include/fdeep/tensor3.hpp
+++ include/fdeep/tensor3.hpp
@@ -404,7 +404,9 @@ inline tensor3 tensor3_from_bytes(const std::uint8_t* value_ptr,
         value_ptr, value_ptr + height * width * channels);
     auto values = fplus::transform([low, high](std::uint8_t b) -> internal::float_type
     {
-        return fplus::reference_interval(low, high, 0.0f, 255.0f,
+        return fplus::reference_interval(low, high,
+            static_cast<float_type>(0.0f),
+            static_cast<float_type>(255.0f),
             static_cast<internal::float_type>(b));
     }, bytes);
     return internal::depth_last_to_depth_first(
@@ -426,7 +428,9 @@ inline void tensor3_to_bytes(const tensor3& t, std::uint8_t* value_ptr,
         [low, high](internal::float_type v) -> std::uint8_t
     {
         return fplus::round<internal::float_type, std::uint8_t>(
-            fplus::reference_interval(0.0f, 255.0f, low, high, v));
+            fplus::reference_interval(
+                static_cast<float_type>(0.0f),
+                static_cast<float_type>(255.0f), low, high, v));
     }, *values);
     for (std::size_t i = 0; i < values->size(); ++i)
     {

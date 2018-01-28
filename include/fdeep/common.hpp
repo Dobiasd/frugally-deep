7736d9fae4c9da2fabefc1d226d467aa9a62b8ae
Patrik Huber <patrikhuber@gmail.com>
2018-01-28 22:47:04 +0000

    Fix Eigen include path

--- include/fdeep/common.hpp
+++ include/fdeep/common.hpp
@@ -20,7 +20,7 @@
 #pragma warning( disable : 4706)
 #pragma warning( disable : 4996)
 #endif
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 #if defined _MSC_VER
 #pragma warning( pop )
 #endif

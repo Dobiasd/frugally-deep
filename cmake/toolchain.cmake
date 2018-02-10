e4a4832897aa9bb70c9707734618d99e8cdb18a1
David Hirvonen <dhirvonen@elucideye.com>
2018-02-10 16:07:55 -0500

    add relocatable cmake generated pkgconfig installation
    This PR mirrors cmake pkgconfig changes from https://github.com/Dobiasd/FunctionalPlus/pull/116 in this repository.
    
    * add pkgconfig boilerplate: https://github.com/forexample/package-example
    * add a logical build target via add_library(fdeep INTERFACE), and provide matching target_include_directories(…) for internal headers
    * move compiler settings to optional (FDEEP_USE_TOOLCHAIN) internal toolchain.cmake file (allow user to override with consistent top level multi-package toolchain)
    * move PROJECT_VERSION to project( … VERSION … )
    * rename UNITTEST to FDEEP_BUILD_UNITTTEST
    * add FDEEP_BUILD_FULL_TEST cmake option to toggle support for full tests so it can be disabled on slow machines (even when not using travis)
    * add FDEEP_USE_DOUBLE option w/ matching target_compile_definitions for sticky option in installed product
    * add top level CMake CONFIG mode package use
      + find_package(XYZ CONFIG REQUIRED) # where XYZ == FunctionalPlus,Eigen,nlohmann_json,Threads
      + target_link_libraries(fdeep INTERFACE Threads::Threads FunctionalPlus::fplus Eigen3::Eigen nlohmann_json)
    * add explicit input file dependency to (generated) input test file for each add_test() so they will run directly ctest as `cd build_tree && ctest` in addition to the `unittest` target
    * update unit tests to use doctest::doctest as explicit CONFIG mode dependency: (same pkgconfig stuff used here https://github.com/onqtam/doctest/blob/b40b7e799deabac916d631d181a7f19f3060acc5/CMakeLists.txt#L39-L82)
    * update README + travis
    * add hunter.cmake file for optional hunter use (default HUNTER_ENABLED=OFF)
    * .gitignore : (*~|_*)

---
+++ cmake/toolchain.cmake
@@ -0,0 +1,16 @@
+add_compile_options(-Wall
+  -Wextra
+  -pedantic
+  -Werror
+  -Weffc++
+  -Wconversion
+  -Wsign-conversion
+  -Wctor-dtor-privacy
+  -Wreorder
+  -Wold-style-cast
+  -Wparentheses
+  )
+
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)

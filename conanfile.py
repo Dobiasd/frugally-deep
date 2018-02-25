3fd04faa05e67491300c520112406b68bb0152ee
Dobiasd <editgym@gmail.com>
2018-02-25 16:36:21 +0100

    add methods source and package_id to class in conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile
-
+import os
 
 class FrugallyDeepConan(ConanFile):
     name = "frugally-deep"
@@ -11,7 +11,15 @@ class FrugallyDeepConan(ConanFile):
                 "functionalplus/0.2@conan/stable",
                 "jsonformoderncpp/3.1.0@vthiery/stable")
 
+    def source(self):
+        source_url = ("%s/archive/v%s.zip" % (self.url, self.version))
+        tools.get(source_url)
+        os.rename("%s-%s" % (self.name, self.version), "sources")
+
     def package(self):
         self.copy("*LICENSE", dst="licenses")
         self.copy("*.h", src=".")
         self.copy("*.hpp", src=".")
+
+    def package_id(self):
+        self.info.header_only()

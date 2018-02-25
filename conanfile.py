6400e13efb6ca0c0d3655f7ccd4226b53c5241d1
Dobiasd <editgym@gmail.com>
2018-02-25 12:47:35 +0100

    conan dependencies

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,9 @@ class FrugallyDeepConan(ConanFile):
     url = "https://github.com/Dobiasd/frugally-deep"
     description = "Header-only library for using Keras models in C++."
     exports_sources = ["include*", "LICENSE"]
+    requires = ("eigen/3.3.4@conan/stable",
+                "functionalplus/0.2@conan/stable",
+                "jsonformoderncpp/3.1.0@vthiery/stable")
 
     def package(self):
         self.copy("*LICENSE", dst="licenses")

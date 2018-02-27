2a94f731d7bd52cb0bae1091e24e48a6404efaf4
Dobiasd <editgym@gmail.com>
2018-02-27 09:37:55 +0100

    fix Conan version references

--- conanfile.py
+++ conanfile.py
@@ -4,25 +4,15 @@ import os
 
 class FrugallyDeepConan(ConanFile):
     name = "frugally-deep"
-    version = "0.1-p0"
     license = "The MIT License (MIT)"
     url = "https://github.com/Dobiasd/frugally-deep"
     description = "Use Keras models in C++ with ease"
-    no_copy_source = True
-    exports_sources = ["LICENSE"]
+    exports_sources = ["include*", "LICENSE"]
     requires = ("eigen/3.3.4@conan/stable",
-                "functionalplus/0.2@conan/stable",
+                "functionalplus/v0.2-p1@dobiasd/stable",
                 "jsonformoderncpp/3.1.0@vthiery/stable")
-
-    def source(self):
-        source_url = ("%s/archive/v%s.zip" % (self.url, self.version))
-        tools.get(source_url)
-        os.rename("%s-%s" % (self.name, self.version), "sources")
 
     def package(self):
         self.copy("*LICENSE*", dst="licenses", src="sources")
         self.copy("*.h", dst=".", src="sources")
         self.copy("*.hpp", dst=".", src="sources")
-
-    def package_id(self):
-        self.info.header_only()

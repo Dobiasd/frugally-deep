e9bb566edb20f901b2e24028e3565d2b5c4dd79a
Dobiasd <editgym@gmail.com>
2018-05-01 14:54:14 +0200

    disallow sum == 0 in softmax layer, fixes #40

--- include/fdeep/layers/softmax_layer.hpp
+++ include/fdeep/layers/softmax_layer.hpp
@@ -41,10 +41,7 @@ protected:
                 {
                     sum += output.get(z_class, y, x);
                 }
-                if (sum == 0)
-                {
-                    sum = std::numeric_limits<float_type>::min();
-                }
+                assertion(sum != 0, "Invalid divisor in softmax layer. Try using\n#define FDEEP_FLOAT_TYPE double\nbefore\n#include \"fdeep/fdeep.hpp\"");
                 // Divide the unnormalized values of each pixel by the stacks sum.
                 for (size_t z_class = 0; z_class < input.shape().depth_; ++z_class)
                 {

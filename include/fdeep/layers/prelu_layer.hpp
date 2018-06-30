01c1f67aa12a4b6062663abc6a5fa214d2329aeb
PhilippKopp <philipp@koppmaps.de>
2018-06-30 18:28:35 +0200

    Adapt PReLU layer to use shared_axes

--- include/fdeep/layers/prelu_layer.hpp
+++ include/fdeep/layers/prelu_layer.hpp
@@ -25,9 +25,13 @@ protected:
     std::vector<std::size_t> shared_axes_;
     tensor3s apply_impl(const tensor3s& input) const override
     {
-        fdeep::tensor3 alpha_tensor3(input[0].shape(), alpha_);
-        fdeep::tensor3 out(input[0].shape(), 1.0f);
+        bool height_shared = std::find(shared_axes_.begin(), shared_axes_.end(), 1) != shared_axes_.end();
+        bool width_shared = std::find(shared_axes_.begin(), shared_axes_.end(), 2) != shared_axes_.end();
+        bool channels_shared = std::find(shared_axes_.begin(), shared_axes_.end(), 3) != shared_axes_.end();
+        size_t width = width_shared ? 1 : input[0].shape().width_;
+        size_t height = height_shared ? 1 : input[0].shape().height_;
 
+        fdeep::tensor3 out(input[0].shape(), 1.0f);
         for (std::size_t z = 0; z < out.shape().depth_; ++z)
         {
             for (std::size_t y = 0; y < out.shape().height_; ++y)
@@ -40,7 +44,11 @@ protected:
                     }
                     else
                     {
-                        out.set(z, y, x, alpha_tensor3.get(z, y, x) * input[0].get(z, y, x));
+                        size_t y_temp = height_shared ? 0 : y;
+                        size_t x_temp = width_shared ? 0 : x;
+                        size_t z_temp = channels_shared ? 0 : z;
+                        size_t pos = z_temp * height * width + y_temp * width + x_temp;
+                        out.set(z, y, x, alpha_->at(pos) * input[0].get(z, y, x));
                     }
                 }
             }

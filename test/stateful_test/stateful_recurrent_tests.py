e5cf17fbd4ea097991551f51b44b781ba6f577e6
Dobiasd <editgym@gmail.com>
2019-10-30 15:19:08 +0100

    Add debug logging to Travis script

--- test/stateful_test/stateful_recurrent_tests.py
+++ test/stateful_test/stateful_recurrent_tests.py
@@ -214,7 +214,7 @@ for h5_fname in model_file_names:
     cmd = 'python3 ../../keras_export/convert_model.py ' + h5_fname + ' ' + json_fname
     os.system(cmd)
 
-os.system('./stateful_tests_cpp')
+os.system('./stateful_recurrent_tests_cpp')
 
 frugally_deep_results = np.fromfile('models/fd_results.bin', dtype=np.float32)
 

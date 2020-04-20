8ab27427e68a1f6bba4be1379f41edb07d729abd
Dobiasd <editgym@gmail.com>
2020-04-20 14:48:18 +0200

    Remove memory sharing of mutable states

--- include/fdeep/recurrent_ops.hpp
+++ include/fdeep/recurrent_ops.hpp
@@ -94,8 +94,8 @@ inline tensors lstm_impl(const tensor& input,
     const MappedRowMajorMatrixXf U = eigen_row_major_mat_from_shared_values(n_units, n_units * 4, const_cast<float_type*>(recurrent_weights.data()));
 
     // initialize cell output states h, and cell memory states c for t-1 with initial state values
-    MappedRowMajorMatrixXf h = eigen_row_major_mat_from_shared_values(1, n_units, const_cast<float_type*>(initial_state_h.as_vector()->data()));
-    MappedRowMajorMatrixXf c = eigen_row_major_mat_from_shared_values(1, n_units, const_cast<float_type*>(initial_state_c.as_vector()->data()));
+    RowMajorMatrixXf h = eigen_row_major_mat_from_values(1, n_units, *initial_state_h.as_vector());
+    RowMajorMatrixXf c = eigen_row_major_mat_from_values(1, n_units, *initial_state_c.as_vector());
 
     std::size_t n_timesteps = input.shape().width_;
     std::size_t n_features = input.shape().depth_;
@@ -160,6 +160,10 @@ inline tensors lstm_impl(const tensor& input,
         lstm_result.push_back(state_c);
     }
 
+    // Copy the final state back into the initial state in the event of a stateful LSTM call
+    initial_state_h = tensor(tensor_shape(n_units), eigen_row_major_mat_to_values(h));
+    initial_state_c = tensor(tensor_shape(n_units), eigen_row_major_mat_to_values(c));
+
     return lstm_result;
 }
 
@@ -200,7 +204,7 @@ inline tensors gru_impl(const tensor& input,
 
     // initialize cell output states h
     // RowVector<Dynamic> h(1, n_units);
-    MappedRowMajorMatrixXf h = eigen_row_major_mat_from_shared_values(1, n_units, const_cast<float_type*>(initial_state_h.as_vector()->data()));
+    RowMajorMatrixXf h = eigen_row_major_mat_from_values(1, n_units, *(initial_state_h.as_vector()));
 
     // use input as eigen matrix of shape (timesteps, n_features)
     const MappedRowMajorMatrixXf x = eigen_row_major_mat_from_shared_values(n_timesteps, n_features, const_cast<float_type*>(input.as_vector()->data()));
@@ -282,6 +286,9 @@ inline tensors gru_impl(const tensor& input,
             gru_result.push_back(state_h);
         }
     }
+
+    // Copy the final state back into the initial state in the event of a stateful GRU call
+    initial_state_h = tensor(tensor_shape(n_units), eigen_row_major_mat_to_values(h));
 
     return gru_result;
 }

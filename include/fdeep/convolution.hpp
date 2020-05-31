bbc323b6accd267a39eeb568b2da08177f0bcf18
Dobiasd <editgym@gmail.com>
2020-05-31 07:01:26 +0200

    Map directly into output tensor using OuterStride

--- include/fdeep/convolution.hpp
+++ include/fdeep/convolution.hpp
@@ -112,23 +112,11 @@ inline tensor5 convolve_accumulative(
             for (std::size_t x = 0; x < f_width; ++x)
             {
                 const float_type* input_ptr = &in.get_ref(0, 0, y + y_filt, x, 0);
-                //float_type* output_ptr = &output.get_ref(0, 0, y_out, x, 0);
-                //Eigen::Map<Eigen::Matrix<float_type, Eigen::OuterStride<>, Eigen::Dynamic>, Eigen::Unaligned> output_map(output_ptr, 1, static_cast<EigenIndex>(out_depth), Eigen::OuterStride<>(f_width));
-                //output_map.noalias() += input * filter;
                 EigenIndex times = static_cast<EigenIndex>((in.shape().width_ - x) / f_width);
                 Eigen::Map<Eigen::Matrix<float_type, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>, Eigen::Unaligned> input(const_cast<float_type*>(input_ptr), times, static_cast<EigenIndex>(f_width * f_depth));
-                const ColMajorMatrixXf output_mat = input * filter;
-                assertion(output_mat.rows() == times, "asd");
-                assertion(output_mat.cols() == static_cast<EigenIndex>(out_depth), "asd");
-                for (EigenIndex i = 0; i < times; ++i)
-                {
-                    for (EigenIndex z = 0; z < static_cast<EigenIndex>(out_depth); ++z)
-                    {
-                        std::size_t x_i = static_cast<std::size_t>(i) * f_width + x;
-                        assertion(x_i < output.shape().width_, "asdasd");
-                        output.get_ref(0, 0, y_out, x_i, static_cast<std::size_t>(z)) += output_mat(i, z);
-                    }
-                }
+                float_type* output_ptr = &output.get_ref(0, 0, y_out, x, 0);
+                Eigen::Map<Eigen::Matrix<float_type, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>, Eigen::Unaligned, Eigen::OuterStride<>> output_map(output_ptr, times, static_cast<EigenIndex>(out_depth), Eigen::OuterStride<>(static_cast<EigenIndex>(f_width * out_depth)));
+                output_map.noalias() += input * filter;
             }
         }
     }

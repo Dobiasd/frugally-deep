0b4861a9b2c0e1b7988303c907d100d685f1e1cb
Tobias Hermann <editgym@gmail.com>
2021-08-26 11:31:09 +0200

    Add support for negative_slope and threshold in ReLU layers (#300)

--- include/fdeep/layers/relu_layer.hpp
+++ include/fdeep/layers/relu_layer.hpp
@@ -17,8 +17,14 @@ namespace fdeep { namespace internal
 class relu_layer : public activation_layer
 {
 public:
-    explicit relu_layer(const std::string& name, const float_type max_value)
-        : activation_layer(name), max_value_(max_value)
+    explicit relu_layer(const std::string& name,
+        const float_type max_value,
+        const float_type negative_slope,
+        const float_type threshold)
+        : activation_layer(name),
+        max_value_(max_value),
+        negative_slope_(negative_slope),
+        threshold_(threshold)
     {
     }
 protected:
@@ -26,11 +32,17 @@ protected:
     {
         auto activation_function = [&](float_type x) -> float_type
         {
-            return std::min<float_type>(std::max<float_type>(x, 0), max_value_);
+            if (x >= max_value_)
+                return max_value_; 
+            if (threshold_ <= x && x < max_value_)
+                return x;
+            return negative_slope_ * (x - threshold_);
         };
         return transform_tensor(activation_function, in_vol);
     }
     float_type max_value_;
+    float_type negative_slope_;
+    float_type threshold_;
 };
 
 } } // namespace fdeep, namespace internal

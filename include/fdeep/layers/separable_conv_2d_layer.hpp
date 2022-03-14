c580d88c0e7daa00420086a36ff238cedc37f19d
Tobias Hermann <editgym@gmail.com>
2022-03-14 16:26:57 +0100

    Refactoring: let separable_conv_2d_layer use depthwise_conv_2d_layer internally for the first step, issue #207 (#333)

--- include/fdeep/layers/separable_conv_2d_layer.hpp
+++ include/fdeep/layers/separable_conv_2d_layer.hpp
@@ -35,52 +35,24 @@ public:
             const float_vec& bias_0,
             const float_vec& bias)
         : layer(name),
-        filters_depthwise_(fplus::transform(generate_im2col_single_filter_matrix,
-            generate_filters(dilation_rate, filter_shape,
-                input_depth, depthwise_weights, bias_0))),
+        depthwise_layer_(name + "_depthwise_part", input_depth,
+            filter_shape, k, strides, p, dilation_rate,
+            depthwise_weights, bias_0),
         filters_pointwise_(generate_im2col_filter_matrix(
             generate_filters(shape2(1, 1),
-                tensor_shape(input_depth), k, pointwise_weights, bias))),
-        strides_(strides),
-        padding_(p)
+                tensor_shape(input_depth), k, pointwise_weights, bias)))
     {
-        assertion(k > 0, "needs at least one filter");
-        assertion(filter_shape.volume() > 0, "filter must have volume");
-        assertion(strides.area() > 0, "invalid strides");
-        assertion(filters_depthwise_.size() == input_depth,
-            "invalid number of filters");
     }
 protected:
     tensors apply_impl(const tensors& inputs) const override
     {
-        const auto& input = single_tensor_from_tensors(inputs);
-
-        const auto input_slices = tensor_to_depth_slices(input);
-
-        assertion(input_slices.size() == filters_depthwise_.size(),
-            "invalid input depth");
-
-        const auto convolve_slice =
-            [&](const tensor& slice, const convolution_filter_matrices& f) -> tensor
-        {
-            assertion(f.filter_shape_.depth_ == 1, "invalid filter depth");
-            const auto result = convolve(strides_, padding_, f, slice);
-            assertion(result.shape().depth_ == 1, "invalid conv output");
-            return result;
-        };
-
-        assertion(input_slices.size() == filters_depthwise_.size(),
-            "invalid depthwise filter count");
-        const auto temp = concatenate_tensors_depth(fplus::zip_with(
-            convolve_slice, input_slices, filters_depthwise_));
-
-        return {convolve(shape2(1, 1), padding::valid, filters_pointwise_, temp)};
+        const auto temp = depthwise_layer_.apply(inputs);
+        const auto temp_single = single_tensor_from_tensors(temp);
+        return {convolve(shape2(1, 1), padding::valid, filters_pointwise_, temp_single)};
     }
 
-    std::vector<convolution_filter_matrices> filters_depthwise_;
+    depthwise_conv_2d_layer depthwise_layer_;
     convolution_filter_matrices filters_pointwise_;
-    shape2 strides_;
-    padding padding_;
 };
 
 } } // namespace fdeep, namespace internal

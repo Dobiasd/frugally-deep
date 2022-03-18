4201a5b2469b2047ca498cf55a021ed84ba3e640
Dobiasd <editgym@gmail.com>
2022-03-18 18:23:50 +0100

    Improve variable naming

--- include/fdeep/layers/depthwise_conv_2d_layer.hpp
+++ include/fdeep/layers/depthwise_conv_2d_layer.hpp
@@ -34,7 +34,7 @@ public:
             const float_vec& depthwise_weights,
             const float_vec& bias)
         : layer(name),
-        filters_depthwise_(generate_im2col_filter_matrix(
+        filters_(generate_im2col_filter_matrix(
             generate_filters(dilation_rate, filter_shape,
                 input_depth, depthwise_weights, bias))),
         strides_(strides),
@@ -42,22 +42,22 @@ public:
     {
         assertion(filter_shape.volume() > 0, "filter must have volume");
         assertion(strides.area() > 0, "invalid strides");
-        assertion(filters_depthwise_.filter_count_ == input_depth,
+        assertion(filters_.filter_count_ == input_depth,
             "invalid number of filters");
-        assertion(filters_depthwise_.filter_shape_.depth_ == 1,
+        assertion(filters_.filter_shape_.depth_ == 1,
             "invalid filter shape");
     }
 protected:
     tensors apply_impl(const tensors& inputs) const override
     {
         const auto& input = single_tensor_from_tensors(inputs);
-        const auto result = depthwise_convolve(strides_, padding_, filters_depthwise_, input);
+        const auto result = depthwise_convolve(strides_, padding_, filters_, input);
         assertion(result.shape().depth_ == input.shape().depth_,
             "Invalid output shape");
         return {result};
     }
 
-    convolution_filter_matrices filters_depthwise_;
+    convolution_filter_matrices filters_;
     shape2 strides_;
     padding padding_;
 };

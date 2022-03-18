5a6a93e65c51d9af86b582676e57f7b2dcf5bffa
Dobiasd <editgym@gmail.com>
2022-03-18 18:44:26 +0100

    Revert depthwise_conv_2d_layer to slicing approach

--- include/fdeep/layers/depthwise_conv_2d_layer.hpp
+++ include/fdeep/layers/depthwise_conv_2d_layer.hpp
@@ -34,7 +34,7 @@ public:
             const float_vec& depthwise_weights,
             const float_vec& bias)
         : layer(name),
-        filters_(generate_im2col_filter_matrix(
+        filters_(fplus::transform(generate_im2col_single_filter_matrix,
             generate_filters(dilation_rate, filter_shape,
                 input_depth, depthwise_weights, bias))),
         strides_(strides),
@@ -42,22 +42,34 @@ public:
     {
         assertion(filter_shape.volume() > 0, "filter must have volume");
         assertion(strides.area() > 0, "invalid strides");
-        assertion(filters_.filter_count_ == input_depth,
-            "invalid number of filters");
-        assertion(filters_.filter_shape_.depth_ == 1,
-            "invalid filter shape");
+        assertion(filters_.size() == input_depth, "invalid filter shape");
     }
 protected:
     tensors apply_impl(const tensors& inputs) const override
     {
         const auto& input = single_tensor_from_tensors(inputs);
-        const auto result = depthwise_convolve(strides_, padding_, filters_, input);
-        assertion(result.shape().depth_ == input.shape().depth_,
-            "Invalid output shape");
-        return {result};
+
+        const auto input_slices = tensor_to_depth_slices(input);
+
+        assertion(input_slices.size() == filters_.size(),
+            "invalid input depth");
+
+        const auto convolve_slice =
+            [&](const tensor& slice, const convolution_filter_matrices& f) -> tensor
+        {
+            assertion(f.filter_shape_.depth_ == 1, "invalid filter depth");
+            const auto result = convolve(strides_, padding_, f, slice);
+            assertion(result.shape().depth_ == 1, "invalid conv output");
+            return result;
+        };
+
+        assertion(input_slices.size() == filters_.size(),
+            "invalid depthwise filter count");
+        return {concatenate_tensors_depth(fplus::zip_with(
+            convolve_slice, input_slices, filters_))};
     }
 
-    convolution_filter_matrices filters_;
+    std::vector<convolution_filter_matrices> filters_;
     shape2 strides_;
     padding padding_;
 };

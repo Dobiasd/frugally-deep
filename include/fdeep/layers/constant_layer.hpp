fa9cc314aa7f01d2dfcdb9eb49325577ca60d05b
Dobiasd <editgym@gmail.com>
2025-04-28 17:22:37 +0200

    Add support for constant-value layers

---
+++ include/fdeep/layers/constant_layer.hpp
@@ -0,0 +1,33 @@
+// Copyright 2016, Tobias Hermann.
+// https://github.com/Dobiasd/frugally-deep
+// Distributed under the MIT License.
+// (See accompanying LICENSE file or at
+//  https://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "fdeep/layers/layer.hpp"
+
+#include <string>
+
+namespace fdeep {
+namespace internal {
+
+    class constant_layer : public layer {
+    public:
+        explicit constant_layer(const std::string& name, const tensor& constant_tensor)
+            : layer(name)
+            , constant_tensor_(constant_tensor)
+        {
+        }
+
+    protected:
+        tensors apply_impl(const tensors&) const override
+        {
+            return { constant_tensor_ };
+        }
+        tensor constant_tensor_;
+    };
+
+}
+} 

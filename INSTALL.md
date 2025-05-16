ecdad45bbac9b82ec3db80486832c2e24c8fd07e
Tobias Hermann <editgym@gmail.com>
2025-05-16 22:06:10 +0200

    Fix negative_slope for leaky-relu activation from 0.3 to 0.2, fixes #445 (#446)

--- INSTALL.md
+++ INSTALL.md
@@ -63,7 +63,7 @@ Just add a *conanfile.txt* with frugally-deep as a requirement and chose the gen
 
 ```
 [requires]
-frugally-deep/v0.18.1@dobiasd/stable
+frugally-deep/v0.18.2@dobiasd/stable
 
 [generators]
 cmake
--- keras_export/generate_test_models.py
+++ keras_export/generate_test_models.py
@@ -611,6 +611,7 @@ def get_test_model_exhaustive() -> Model:
         Activation('squareplus')(inputs[25]),
         Activation('tanh')(inputs[25]),
         Activation('tanh_shrink')(inputs[25]),
+        Activation('leaky_relu')(inputs[25]),
         LeakyReLU(name="real_LeakyReLU_layer", negative_slope=0.5)(inputs[25]),
         ReLU(name="real_ReLU_layer_1")(inputs[25]),
         ReLU(name="real_ReLU_layer_2", max_value=0.4, negative_slope=1.1, threshold=0.3)(inputs[25]),
